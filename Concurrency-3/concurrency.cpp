29924435b98e17f11b996bd3143e9c6568576724
habibelo@oregonstate.edu <habibelo@os2.engr.oregonstate.edu>
2017-11-16 15:11:34 -0800

    Fixed code causing errors. The struct is effectively no longer used. Created semaphore for printing because statements were printing overlapped. Currently, it's not waiting for processes to clear up, but all of the processes are running successfully.

--- Concurrency-3/concurrency.cpp
+++ Concurrency-3/concurrency.cpp
@@ -2,12 +2,16 @@
 *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-
+#define MAX_PROCESSES 3
 
 #include <iostream>
 #include <pthread.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
 
 using std::cout;
 using std::endl;
@@ -19,25 +23,56 @@ struct IndvThread{
 	void* action;
 };
 
+sem_t empty;
+sem_t full_slots;
+sem_t mutex;
+sem_t printer;
+int sval;
+
+//int num_processes = 0;
 
 /*
 	* Outputs the thread number of the thread whose working
 */
 void working(const int &workerNum){
-	cout << workerNum << " is working" << endl;
+    int waittime = (rand() % 4) + 1;
+    sem_wait(&printer);
+    cout << workerNum << " is working" << endl;
+    cout << "Wait time: " << waittime << endl;
+    sem_post(&printer);
+    sleep(waittime);
+    sem_post(&full_slots);
 }
 /*
 	* Outputs the thread number of the thread whose waiting
 */
 void waiting(const int &workerNum){
+    sem_wait(&printer);
 	cout << workerNum << " is waiting" << endl;
+    sem_post(&printer);
 }
 /*
 	* The threads base function
 	* Decides whether the thread can work yet or not
 */
-void begin(void* worker){
-		
+void *begin(void *worker){
+    intptr_t threadNum = (intptr_t) worker;
+    sem_getvalue(&full_slots, &sval);
+    if (sval == MAX_PROCESSES) {
+        sem_wait(&mutex);
+        waiting(threadNum);
+        while (sval > 0) {
+            sem_wait(&full_slots);
+            sem_getvalue(&full_slots, &sval);
+        }
+        sem_post(&mutex);
+        sem_wait(&full_slots);
+        working(threadNum);
+    }
+    else {
+        working(threadNum);
+    }
+    return NULL;
 }
 /*
 	* Set up the workers
@@ -45,7 +80,7 @@ void begin(void* worker){
 void initWorkers(struct IndvThread workers[], const int &numThreads){
 	for(int i = 0; i < numThreads; i++){
 		workers[i].threadNum = i;
-		workers[i].action = waiting;
+		workers[i].action = (void *) waiting;
 	}
 }
 
@@ -63,15 +98,29 @@ void cleanUp(pthread_t threads[], struct IndvThread workers[], const int &numThr
 	* Main function
 */
 int main(int argc, char *argv[]){
+    
+    srand( time(NULL) );
+    
+    // INITIALIZE SEMAPHORES //
+    sem_init(&empty, 0, 1);
+    sem_init(&full_slots, 0, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&printer, 0, 1);
+    
 	if(argc != 2){
+        sem_wait(&printer);
 		cout << "Wrong Format" << endl; 
 		cout << "Correct Format: concurrency <num threads>" << endl;
+        sem_post(&printer);
 	}
 	else{
 		srand(time(NULL));
 		int numThreads = atoi(argv[1]);
+        intptr_t i = 0;
 		if(numThreads < 1){
+            sem_wait(&printer);
 			cout << "Please choose at least one thread" << endl;
+            sem_post(&printer);
 		}
 		else{
 			/* Set up structures */
@@ -79,12 +128,15 @@ int main(int argc, char *argv[]){
 			pthread_t* threads = new pthread_t[numThreads];
 			initWorkers(workers, numThreads);
 			/* Start threads */ 
-			for(int i = 0; i < numThreads; i++){
-				if( pthread_create(&threads[i], NULL, begin, (void*) &workers[i]) ){
+			for(i = 0; i < numThreads; i++){
+                //cout << "i: " << i << endl;
+				if( pthread_create(&threads[i], NULL, begin, (void *) i) < 0 ){
+                    sem_wait(&printer);
 					cout << "Error: "
-						<< workers[i].threadNum
-						<< " thread could not be initialised ... exiting now"
-						<< endl;
+						 << workers[i].threadNum
+						 << " thread could not be initialised ... exiting now"
+						 << endl;
+                    sem_post(&printer);
 				}
 			}
 			cleanUp(threads, workers, numThreads);
