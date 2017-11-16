a14b1e5ec59e012aa1030484dfee06e3f1ae6afd
fischjer4 <fischjer4@gmail.com>
2017-11-15 16:18:14 -0800

    created Concurrency-3 fodler

---
+++ Concurrency-3/concurrency.cpp
@@ -0,0 +1,22 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+#include <iostream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+/*
+	* Main function
+*/
+int main(int argc, char *argv[]){
+
+	return 0;
+}
