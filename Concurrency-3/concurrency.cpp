0e6e2b10b0dcc67f72543cbdb12c0e76481bd009
fischjer4 <fischjer4@gmail.com>
2017-11-18 12:30:46 -0800

    finished concurrency part 1. Replaced pthread_cond_signal with pthread_cond_broadcast

--- Concurrency-3/concurrency.cpp
+++ Concurrency-3/concurrency.cpp
@@ -2,7 +2,6 @@
  Sources Used:
  * https://www.ibm.com/support/knowledgecenter/en/ssw_i5_54/apis/users_14.htm
  * https://stackoverflow.com/questions/25848615/c-printing-cout-overlaps-in-multithreading
- * https://stackoverflow.com/questions/5724171/passing-an-array-by-reference
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -26,139 +25,84 @@ struct IndvThread{
 	int threadNum;
 	string action;
 	int workWaitTime;
-	// void (*action)(const int&);
 };
 
 sem_t keyHolder;
 pthread_mutex_t printer;
-pthread_mutex_t needAllKeys;
+pthread_mutex_t needAllKeys; 
 pthread_cond_t signalAllKeys;
-int numThreads = 0;
 static int keysOpen;
 static int stop = 0;
-static int done = 0;
-struct IndvThread *workers = NULL;
-
+struct IndvThread* workers = NULL;
 
 /*
-	* Outputs the thread number of the thread whose working
+	* Prints the status of all threads
 */
-/*void working(const int &workerNum){
-    pthread_mutex_lock(&printer);
-		int workTime = rand() % 10 + 1;
-    	cout << "Thread " << workerNum << " is working for "<< workTime << "..." << endl;
-    pthread_mutex_unlock(&printer);
-    sleep(workTime);
-}*/
-/*
-	* Outputs the thread number of the thread whose waiting
-*/
-/*void waiting(const int &workerNum){
-    pthread_mutex_lock(&printer);
-		cout << "Thread " << workerNum << " is waiting..." << endl;
-    pthread_mutex_unlock(&printer);
-}*/
-
-void checkAllDone(struct IndvThread workers[])
-{
-    for (int i = 0; i < numThreads; i++) {
-        if (workers[i].action != "done") {
-            done = 0;
-        }
-        else {
-            done = 1;
-        }
-    }
-}
-
-/*
-    * Prints the status of all threads
- */
-void* printAll(void* num)
-{
+void* printAll(void* num){
 	int numWorkers = *(int*)num;
 	if(workers != NULL){
 		while(true){
-            checkAllDone(workers);
-    		sem_getvalue(&keyHolder, &keysOpen);
 		    pthread_mutex_lock(&printer);
 				cout << "Keys open: " << keysOpen << endl;
 		    pthread_mutex_unlock(&printer);
 			for(int i = 0; i < numWorkers; i++){
 		    	pthread_mutex_lock(&printer);
 					if(workers[i].action == "working"){
-						cout << "Thread " << i << " is working for "
-                             << workers[i].workWaitTime << " seconds..." << endl;
-                        workers[i].workWaitTime--;
-                        if (workers[i].workWaitTime == 0) {
-                            workers[i].action = "done";
-                        }
+						cout << "Thread " << i << " is working for "<< workers[i].workWaitTime << "..." << endl;
 					}
 					else{
-						cout << "Thread " << i << " is " << workers[i].action
-                             << "..." << endl;
+						cout << "Thread " << i << " is " << workers[i].action << "..." << endl;
 					}
 		    	pthread_mutex_unlock(&printer);
 			}
 			cout << endl;
-            if (done == 1) {
-                break;
-            }
 			/* Print every 1 second */
 			sleep(1);
 		}
 	}
 	return NULL;
 }
-
 /*
 	* The threads base function
 	* Decides whether the thread can work yet or not
 */
-void* begin(void *worker)
-{
+void* begin(void *worker){
     struct IndvThread* curWorker = (struct IndvThread*)worker;
 	while(true){
-    	sem_getvalue(&keyHolder, &keysOpen);			
-        if(keysOpen == 0) {
-            stop = 1;
-        }
-        
-		// /* see how many keys are available */
+		/* if thread needs to wait till all keys are returned, then wait */
 		if(stop){
-			pthread_mutex_lock(&needAllKeys);					
-				pthread_cond_wait(&signalAllKeys, &needAllKeys);
-                curWorker->action = "waiting";
+			pthread_mutex_lock(&needAllKeys);	
+				while(stop){
+					pthread_cond_wait(&signalAllKeys, &needAllKeys);
+				}
 			pthread_mutex_unlock(&needAllKeys);	
 		}
-
-        else {
-            // The process is now working //
-            sem_wait(&keyHolder);
-                curWorker->action = "working";
-                curWorker->workWaitTime = rand() % 4+ 1;
-                sleep(curWorker->workWaitTime);
-                curWorker->action = "waiting";
-            sem_post(&keyHolder);
-            //curWorker->action = "done"; // the process has now completed its work
-
-            sem_getvalue(&keyHolder, &keysOpen);
-            if(keysOpen == MAX_PROCESSES){
-                pthread_cond_signal(&signalAllKeys);
-                pthread_cond_signal(&signalAllKeys);
-                pthread_cond_signal(&signalAllKeys);
-                stop = 0;
-            }
-        }
+		/* grab a key */
+		sem_wait(&keyHolder);	
+			/* if all keys are taken, then make others wait till all are back */
+			sem_getvalue(&keyHolder, &keysOpen);
+			if(keysOpen == 0){
+				stop = 1;
+			}
+			curWorker->action = "working";
+			curWorker->workWaitTime = rand() % 13 + 1;
+			sleep(curWorker->workWaitTime);
+			curWorker->action = "waiting";
+		sem_post(&keyHolder);
+		
+		/* if all keys are back, let go of the lock */
+		sem_getvalue(&keyHolder, &keysOpen);
+		if(keysOpen == MAX_PROCESSES){
+			pthread_cond_broadcast(&signalAllKeys);
+			stop = 0;
+		}
 	}
     return NULL;
 }
-
 /*
 	* Set up the workers
 */
-void initWorkers(struct IndvThread workers[], const int &numThreads)
-{
+void initWorkers(struct IndvThread workers[], const int &numThreads){
 	for(int i = 0; i < numThreads; i++){
 		workers[i].threadNum = i;
 		workers[i].action = "waiting";
@@ -167,30 +111,25 @@ void initWorkers(struct IndvThread workers[], const int &numThreads)
 /*
 	* Sets up the semaphore an mutexes
 */
-void initLocks()
-{
+void initLocks(){
 	sem_init(&keyHolder, 0, 3);
     pthread_mutex_init(&printer, NULL);
     pthread_mutex_init(&needAllKeys, NULL);
 	pthread_cond_init(&signalAllKeys, NULL);
 }
-
 /*
 	* Frees all allocated memory
 */
-void freeMemory(pthread_t threads[], struct IndvThread workers[])
-{
+void freeMemory(pthread_t threads[], struct IndvThread workers[]){
 	if(threads != NULL)
 		delete [] threads;
 	if(workers != NULL)		
 		delete [] workers;
 }
-
 /*
 	* Joins all running threads
 */
-void joinThreads(pthread_t threads[], const int &numThreads)
-{
+void joinThreads(pthread_t threads[], const int &numThreads){
 	for(int i = 0; i < numThreads; i++){
 		pthread_join(threads[i], NULL);
 	}
@@ -203,8 +142,8 @@ void cleanUp(pthread_t threads[], struct IndvThread workers[], const int &numThr
 /*
 	* Main function
 */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+    
     srand( time(NULL) );
 
 	if(argc != 2){
@@ -212,7 +151,7 @@ int main(int argc, char *argv[])
 		cout << "Correct Format: concurrency <num threads>" << endl;
 	}
 	else{
-		numThreads = atoi(argv[1]);
+		int numThreads = atoi(argv[1]);
 		if(numThreads < 4){
 			cout << "Please choose at least four threads" << endl;
 		}
@@ -227,9 +166,8 @@ int main(int argc, char *argv[])
 			/* Start threads */ 
 			if( pthread_create(&printerThread, NULL, printAll, (void*)&numThreads) < 0){
 				cout << "Error initializing printer thread ... exiting now" << endl;
-                pthread_mutex_unlock(&printer);
-                freeMemory(threads, workers);
-                exit(EXIT_FAILURE);
+				freeMemory(threads, workers);	
+				exit(EXIT_FAILURE);
 			} 
 			else{
 				for(int i = 0; i < numThreads; i++){
